a7d8795bb20b40e5b949081306e95342fae722f7
VasiliyTemniy <flash_er@mail.ru>
2024-10-18 16:30:12 +0300

    move css imports to app root; remove unused prop pass

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import ImageInit from './components/ImageInit';
 import GridOutput from './components/GridOutput';
 import Menu from './components/Menu';
 import { useRef } from 'react';
+import './styles/grid-output.css';
+import './styles/nav.css';
+import './styles/button.css';
+import './styles/input.css';
 
 const App = () => {
 
@@ -16,7 +20,7 @@ const App = () => {
   return (
     <>
       <Menu handleFileSelection={handleFileSelection} gridOutputRef={gridOutputRef} />
-      <ImageInit handleFileSelection={handleFileSelection} />
+      <ImageInit />
       <GridOutput ref={gridOutputRef} />
     </>
   );
