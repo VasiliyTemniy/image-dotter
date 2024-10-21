f6c9094421235d68035bd548fa5a3fa128de0ea7
VasiliyTemniy <flash_er@mail.ru>
2024-10-21 15:37:15 +0300

    add some jsdoc; switch to aoa of fixed arrays from aoa of objects to lessen output json length; draw rounded lines instead of circles

--- src/utils/dottergrid.js
+++ src/utils/dottergrid.js
@@ -1,3 +1,12 @@
+/**
+ * { color: string, length: number, angle: number, stroke: string, strokeWidth: number }
+ * @typedef {[string, number, number, string, number]} Cell
+ */
+
+/**
+ * @param {File} file
+ * @returns {Promise<Image>}
+ */
 export const readImage = (file) => {
   return new Promise((resolve, reject) => {
     try {
@@ -16,6 +25,10 @@ export const readImage = (file) => {
   });
 };
 
+/**
+ * @param {Image} image
+ * @param {React.MutableRefObject<HTMLCanvasElement>} inputCanvasRef
+ */
 export const drawImage = (image, inputCanvasRef) => {
   // TODO handle something in regards of input image scaling
   // const width = image.width;
@@ -28,6 +41,12 @@ export const drawImage = (image, inputCanvasRef) => {
   contextInput.drawImage(image, 0, 0, canvasInput.width, canvasInput.height);
 };
 
+/**
+ * @param {React.MutableRefObject<HTMLCanvasElement>} inputCanvasRef
+ * @param {React.MutableRefObject<HTMLCanvasElement>} outputCanvasRef
+ * @param {number} rowsCount
+ * @param {number} columnsCount
+ */
 export const drawGridPreview = (inputCanvasRef, outputCanvasRef, rowsCount, columnsCount) => {
   const canvasInput = inputCanvasRef.current;
   const contextInput = canvasInput.getContext('2d', { willReadFrequently: true });
@@ -39,19 +58,27 @@ export const drawGridPreview = (inputCanvasRef, outputCanvasRef, rowsCount, colu
   drawOutputByGrid(grid, rowsCount, columnsCount, 500, 500, contextOutput);
 };
 
+/**
+ * @param {number} rowsNumber
+ * @param {number} columnsNumber
+ * @param {CanvasRenderingContext2D} contextInput
+ * @returns {Cell[][]}
+ */
 export const makeColorGrid = (rowsNumber, columnsNumber, contextInput) => {
+  /** @type {Cell[][]} */
   const grid = [];
   const columnWidth = Math.floor(contextInput.canvas.width / rowsNumber);
   const rowHeight = Math.floor(contextInput.canvas.height / columnsNumber);
 
   for (let row = 0; row < rowsNumber; row++) {
+    /** @type {Cell[]} */
     const gridrow = [];
     for (let column = 0; column < columnsNumber; column++) {
       const x = columnWidth * column;
       const y = rowHeight * row;
       const data = contextInput.getImageData(x, y, columnWidth, rowHeight).data;
       const color = middleweightColor(data);
-      gridrow.push({ color, row, column });
+      gridrow.push([color, 0, 0, '', 0]);
     }
     grid.push(gridrow);
   }
@@ -59,6 +86,14 @@ export const makeColorGrid = (rowsNumber, columnsNumber, contextInput) => {
   return grid;
 };
 
+/**
+ * @param {Cell[][]} grid
+ * @param {number} rowsCount
+ * @param {number} columnsCount
+ * @param {number} outputCanvasWidth
+ * @param {number} outputCanvasHeight
+ * @param {CanvasRenderingContext2D} contextOutput
+ */
 const drawOutputByGrid = (
   grid,
   rowsCount,
@@ -74,11 +109,31 @@ const drawOutputByGrid = (
     for (let column = 0; column < columnsCount; column++) {
       const xOut = outputWidth * column + Math.floor(outputWidth / 2);
       const yOut = outputHeight * row + Math.floor(outputHeight / 2);
-      drawCircle(contextOutput, xOut, yOut, Math.floor(outputWidth / 2) - 1, grid[row][column].color);
+      const color = grid[row][column][0];
+      const length = grid[row][column][3];
+      const angle = grid[row][column][4];
+      const stroke = grid[row][column][5];
+      const strokeWidth = grid[row][column][6];
+      drawRoundedLine(
+        contextOutput,
+        xOut,
+        yOut,
+        Math.floor(outputWidth / 2) - 1,
+        length,
+        angle + Math.PI / 2,
+        color,
+        stroke,
+        strokeWidth,
+      );
     }
   }
 };
 
+/**
+ * Gets middleweight color of some image part by surrounding pixels
+ * @param {ImageData["data"]} data
+ * @returns {string}
+ */
 const middleweightColor = (data) => {
   let red = 0;
   let green = 0;
@@ -99,6 +154,11 @@ const middleweightColor = (data) => {
   return rgba2hex([red, green, blue, alpha]);
 };
 
+/**
+ * RGBA represented as [0-255, 0-255, 0-255, 0-255] to hex
+ * @param {[number, number, number, number]} rgba
+ * @returns {string}
+ */
 export const rgba2hex = (rgba) => {
   return (
     '#' +
@@ -109,9 +169,45 @@ export const rgba2hex = (rgba) => {
   );
 };
 
-const drawCircle = (context, x, y, radius, fill, stroke, strokeWidth) => {
+// /**
+//  * @param {CanvasRenderingContext2D} context
+//  * @param {number} x
+//  * @param {number} y
+//  * @param {number} radius
+//  * @param {string} fill
+//  * @param {string} stroke
+//  * @param {number} strokeWidth
+//  */
+// export const drawCircle = (context, x, y, radius, fill, stroke, strokeWidth) => {
+//   context.beginPath();
+//   context.arc(x, y, radius, 0, 2 * Math.PI, false);
+//   if (fill) {
+//     context.fillStyle = fill;
+//     context.fill();
+//   }
+//   if (stroke) {
+//     context.rowWidth = strokeWidth;
+//     context.strokeStyle = stroke;
+//     context.stroke();
+//   }
+// };
+
+/**
+ * @param {CanvasRenderingContext2D} context
+ * @param {number} x
+ * @param {number} y
+ * @param {number} radius
+ * @param {number} length
+ * @param {number} angle
+ * @param {string} fill
+ * @param {string} stroke
+ * @param {number} strokeWidth
+ */
+const drawRoundedLine = (context, x, y, radius, length, angle, fill, stroke, strokeWidth) => {
   context.beginPath();
-  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  context.arc(x, y, radius, angle, angle + Math.PI, false);
+  context.arc(x + Math.sin(angle) * length, y + Math.cos(angle) * length, radius, angle + Math.PI, angle + 2 * Math.PI, false);
+  context.closePath();
   if (fill) {
     context.fillStyle = fill;
     context.fill();
