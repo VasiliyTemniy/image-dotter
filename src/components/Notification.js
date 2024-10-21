7a804f4c78b21d88c4a9c715e9cd7bd3e676fae1
VasiliyTemniy <flash_er@mail.ru>
2024-10-21 14:13:35 +0300

    upd style handling

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,25 +1,7 @@
 const Notification = ({ message , type, shown }) => {
 
-  // if (message === null || message === '') {
-  //   return null;
-  // }
-
-  // const style = {
-  //   position: 'absolute',
-  //   zIndex: 101,
-  //   color: type === 'error' ? 'red' : 'green',
-  //   background: 'lightgrey',
-  //   fontSize: 20,
-  //   borderStyle: 'solid',
-  //   borderRadius: 5,
-  //   padding: 10,
-  //   marginBottom: 10,
-  //   opacity: 0,
-  //   animation: 'toggle-visibility-notificaion 5s forwards ease-in-out',
-  // };
-
   const style = {
-    color: type === 'error' ? 'red' : 'green',
+    borderColor: type === 'error' ? 'red' : 'green',
     opacity: shown ? 1 : 0,
   };
 
