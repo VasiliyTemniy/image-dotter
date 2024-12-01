6d49d8fe9d3f051823028cf71e5363c0c58f5cea
VasiliyTemniy <flash_er@mail.ru>
2024-12-01 16:21:55 +0300

    add generator test component

---
+++ src/components/GeneratorTestComponent.js
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from 'react';
+import { Generator } from '../utils/generator';
+
+export const GeneratorTestComponent = () => {
+
+  const generatorTestCanvasRef = useRef(null);
+
+  // const possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // const possibleValues = [1, 2, 3];
+  const possibleValues = [1, 2, 3, 400, 5, 6, 7];
+
+
+
+  const generator = new Generator({
+    seed: 98564,
+    possibleValues,
+    // estimated: 2.35,
+    estimatedIndex: 0,
+    testCanvasRef: generatorTestCanvasRef
+  });
+
+  useEffect(() => {
+    generator.test(generatorTestCanvasRef);
+  });
+
+  return (
+    <canvas
+      ref={generatorTestCanvasRef}
+      id='generator-test-canvas'
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: 50000,
+        border: '1px solid red',
+      }}
+    />
+  );
+};
