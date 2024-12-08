8fc5c66a54a531d2616b367b9f0b9bfb754a7ac1
VasiliyTemniy <flash_er@mail.ru>
2024-12-08 22:06:23 +0300

    refactor grid html output component

--- src/components/GridOutput.js
+++ src/components/GridOutput.js
@@ -1,10 +1,25 @@
 import { useState, useEffect, forwardRef, useImperativeHandle, useRef, memo } from 'react';
 
+/**
+ * @typedef {import('../index.d.ts').GridHtmlVisualParams} GridHtmlVisualParams
+ * @typedef {import('../index.d.ts').DotterCell} DotterCell
+ */
+
 const GridOutput = forwardRef((props, refs) => {
   const [grid, setGrid] = useState([]);
+  const [gridHtmlParams, setGridHtmlParams] = useState({});
+  const [_animationParams, setAnimationParams] = useState({});
 
-  const handleCreate = (grid) => {
-    setGrid(grid);
+  const drawHtmlPreview = (gridHtmlParams, animationParams) => {
+    setGridHtmlParams(gridHtmlParams);
+    setAnimationParams(animationParams);
+  };
+
+  const setSize = (width, height) => {
+    const background = backgroundRef.current;
+    if (!background) return;
+    background.style.width = `${width}px`;
+    background.style.height = `${height}px`;
   };
 
   const containerRef = useRef(null);
@@ -12,9 +27,11 @@ const GridOutput = forwardRef((props, refs) => {
 
   useImperativeHandle(refs, () => {
     return {
-      handleCreate,
+      drawHtmlPreview,
+      setSize,
       backgroundRef,
-      grid
+      grid,
+      setGrid
     };
   });
 
@@ -44,53 +61,76 @@ const GridOutput = forwardRef((props, refs) => {
     });
   }, []);
 
-  const containerStyle = {
-    width: '100%',
-    height: '100vh',
-    gap: '1px',
-  };
-
   return (
-    <>
-      <div id="background" className="grid-output__background" ref={backgroundRef} style={containerStyle}>
-        <div id="container" className="grid-output__container" ref={containerRef} style={containerStyle}>
-          <MemoizedGrid grid={grid}/>
+    <div className='container padding-1rem border-black-1px width-fit-content bg-white margin-1rem'>
+      <div className='title'>
+        HTML Output
+      </div>
+      <div id="background" className="grid-output__background" ref={backgroundRef}>
+        <div id="container" className="grid-output__container" ref={containerRef}>
+          <MemoizedGrid grid={grid} gridHtmlParams={gridHtmlParams}/>
         </div>
       </div>
-    </>
-  );
-});
-
-const MemoizedGrid = memo(({ grid }) => {
-  const rowStyle = {
-    height: '8px',
-    //height: '5%',
-    gap: '1px',
-  };
-
-  const cellStyle = {
-    width: '8px',
-    //width: '5%',
-    height: '100%',
-    borderRadius: '1000px',
-  };
-
-  return (
-    <div>
-      {grid.map((row, rowIndex) => (
-        <div key={'grid-' + rowIndex} className="row" style={rowStyle}>
-          {row.map((cell, columnIndex) => (
-            <div
-              key={'r' + rowIndex + 'c' + columnIndex}
-              className="cell"
-              style={{ backgroundColor: cell[3], ...cellStyle }}
-            ></div>
-          ))}
-        </div>
-      ))}
     </div>
   );
 });
+
+const MemoizedGrid = memo(
+  /**
+   * @param {{ grid: DotterCell[][], gridHtmlParams: GridHtmlVisualParams }} props
+   */
+  ({ grid, gridHtmlParams }) => {
+
+    // let baseX = Infinity;
+    // let baseY = Infinity;
+
+    // for (let i = 0; i < grid.length; i++) {
+    //   for (let j = 0; j < grid[i].length; j++) {
+    //     if (grid[i][j][0] < baseX) {
+    //       baseX = grid[i][j][0];
+    //     }
+    //     if (grid[i][j][1] < baseY) {
+    //       baseY = grid[i][j][1];
+    //     }
+    //   }
+    // }
+
+    // console.log('baseX', baseX, 'baseY', baseY);
+
+    const rowStyle = {
+      position: 'relative',
+      marginTop: `${gridHtmlParams.horizontalGapPx}px`,
+      height: `${gridHtmlParams.monoCellSize}px`,
+    };
+
+    const cellStyle = {
+      position: 'absolute',
+      height: '100%',
+      borderRadius: `${gridHtmlParams.borderRadius}px`,
+    };
+
+    return (
+      <>
+        {grid.map((row, rowIndex) => (
+          <div key={'grid-' + rowIndex} className="row" style={{ ...rowStyle }}>
+            {row.map((cell, columnIndex) => (
+              <div
+                key={'r' + rowIndex + 'c' + columnIndex}
+                className="cell"
+                style={{
+                  backgroundColor: cell[3],
+                  ...cellStyle,
+                  left: `${cell[0] * gridHtmlParams.monoCellSize * gridHtmlParams.overrideSpanWidthFactor + gridHtmlParams.verticalGapPx}px`,
+                  // left: `${getCellRelativeX(cell, baseX, row, columnIndex) * gridHtmlParams.monoCellSize + gridHtmlParams.verticalGapPx}px`,
+                  width: `${cell[2] * gridHtmlParams.monoCellSize * gridHtmlParams.overrideSpanWidthFactor - gridHtmlParams.verticalGapPx}px`
+                }}
+              ></div>
+            ))}
+          </div>
+        ))}
+      </>
+    );
+  });
 
 MemoizedGrid.displayName = 'MemoizedGrid';
 
